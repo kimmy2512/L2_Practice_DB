04def554367ffdf4c5d252a9206933720ebd1672
kimmy2512 <51727300+kimmy2512@users.noreply.github.com>
2020-08-04 12:12:18 +1200

    fixed image

---
+++ L2_DB_Prac/config.php
@@ -0,0 +1,11 @@
+<?php 
+
+ini_set('display_errors', 'On'); // change to 'Off' when live
+
+define ("DB_NAME","kime69800");
+define ("DB_USERNAME","_kime69800");
+define ("DB_PASSWORD","706e39ec240571b7fa02f74460936d39");
+define ("DB_HOST","localhost");
+
+?>
+
---
+++ L2_DB_Prac/index.php
@@ -0,0 +1,32 @@
+<?php include("topbit.php") ?>
+
+        <div class="box main">
+            <h2>Welcome</h2>
+            
+            
+            <p>
+                Put link to original dataset here
+            </p>
+            
+
+            
+        </div> <!-- / main -->
+        
+        <div class="box side">
+           
+           <h2>Search Area</h2>
+           
+           <p>This is where the search area goes
+
+            
+        </div> <!-- / side bar -->
+        
+        <div class="box footer">
+            CC GTT 20XX
+        </div> <!-- / footer -->
+                
+        
+    </div> <!-- / wrapper -->
+    
+            
+</body>
