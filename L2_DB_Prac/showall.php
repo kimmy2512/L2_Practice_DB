91f4b1b65b78b25571e8ee3d4243ca69057250bb
kimmy2512 <51727300+kimmy2512@users.noreply.github.com>
2020-08-06 09:34:45 +1200

    complete website before refinements

--- L2_DB_Prac/showall.php
+++ L2_DB_Prac/showall.php
@@ -53,7 +53,13 @@
                     
                     <b>Developer</b>:
                     <?php echo $find_rs['DevName'] ?>
+                    
+                    <br />
+                    <b>Rating</b>: <?php echo $find_rs['User Rating'] ?> (based on <?php echo $find_rs['Rating Count'] ?> votes)
+                    
                 </p>
+                <hr />
+                <?php echo $find_rs['Description'] ?>
                 
             </div>  <!-- / results -->
             
