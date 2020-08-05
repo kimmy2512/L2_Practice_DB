da1fb43fe4703725790db6ca107dbf7968e0974f
kimmy2512 <51727300+kimmy2512@users.noreply.github.com>
2020-08-06 09:26:42 +1200

    Working genre and developer display

--- L2_DB_Prac/index.php
+++ L2_DB_Prac/index.php
@@ -5,28 +5,11 @@
             
             
             <p>
-                Put link to original dataset here
+                This database includes some of the most popular apps available for mobile devices. The <a href="https://www.kaggle.com/tristan581/17k-apple-app-store-strategy-games/version/1">original dataset</a> can be found at the preceding link.
             </p>
             
 
             
         </div> <!-- / main -->
         
-        <div class="box side">
-           
-           <h2>Search Area</h2>
-           
-           <p>This is where the search area goes
-
-            
-        </div> <!-- / side bar -->
-        
-        <div class="box footer">
-            CC GTT 20XX
-        </div> <!-- / footer -->
-                
-        
-    </div> <!-- / wrapper -->
-    
-            
-</body>
+    <?php include("bottombit.php") ?>
---
+++ L2_DB_Prac/showall.php
@@ -0,0 +1,77 @@
+<?php include("topbit.php");
+
+    $find_sql = "SELECT * FROM `L2_prac_game_details` 
+    JOIN `L2_prac_genre` ON (L2_prac_game_details.GenreID = L2_prac_genre.GenreID)
+    JOIN `L2_prac_developer` ON (L2_prac_game_details.DeveloperID = L2_prac_developer.DeveloperID)
+    
+    ";
+    $find_query = mysqli_query($dbconnect, $find_sql);
+    $find_rs = mysqli_fetch_assoc($find_query);
+    $count = mysqli_num_rows($find_query);
+
+?>
+
+        <div class="box main">
+            <h2>All Results</h2>
+            
+            
+            <?php
+            
+            if($count < 1) {
+                
+                ?>
+            
+            <div class ="error">
+            
+                Sorry! Thereare no results that match your search. Please use the search box int he side bar to try again. 
+            
+            </div>  <!-- end error -->
+            
+            <?php
+            } // end no results if
+            
+            else {
+                do 
+                {
+                    
+                    ?>
+            
+            <!-- Results go here -->
+            <div class="results">
+                <span class="sub_heading">
+                    <a href="<?php echo $find_rs['URL']; ?>">
+                        <?php echo $find_rs['Name']; ?>
+                    </a>
+                </span> - <?php echo $find_rs['Subtitle'] ?>
+                
+                <p>
+                
+                    <b>Genre</b>:
+                    <?php echo $find_rs['Genre'] ?>
+                    
+                    <br />
+                    
+                    <b>Developer</b>:
+                    <?php echo $find_rs['DevName'] ?>
+                </p>
+                
+            </div>  <!-- / results -->
+            
+            <br />
+            
+            <?php
+                    
+                } // end results 'do'
+                
+                while
+                    ($find_rs=mysqli_fetch_assoc($find_query));
+                
+            } // end else
+            
+            ?>
+            
+
+            
+        </div> <!-- / main -->
+        
+    <?php include("bottombit.php") ?>
