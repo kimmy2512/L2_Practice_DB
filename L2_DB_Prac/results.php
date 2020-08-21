970e5973387fd2c96afb73d664feaa16507e3f8a
kimmy2512 <51727300+kimmy2512@users.noreply.github.com>
2020-08-21 20:03:45 +1200

    working star rating and new results.php file

--- L2_DB_Prac/results.php
+++ L2_DB_Prac/results.php
@@ -1,21 +1,4 @@
-<?php include("topbit.php");
-
-    $find_sql = "SELECT * FROM `L2_prac_game_details` 
-    JOIN `L2_prac_genre` ON (L2_prac_game_details.GenreID = L2_prac_genre.GenreID)
-    JOIN `L2_prac_developer` ON (L2_prac_game_details.DeveloperID = L2_prac_developer.DeveloperID)
-    
-    ";
-    $find_query = mysqli_query($dbconnect, $find_sql);
-    $find_rs = mysqli_fetch_assoc($find_query);
-    $count = mysqli_num_rows($find_query);
-
-?>
-
-        <div class="box main">
-            <h2>All Results</h2>
-            
-            
-            <?php
+<?php
             
             if($count < 1) {
                 
@@ -147,8 +130,3 @@
             
             ?>
             
-
-            
-        </div> <!-- / main -->
-        
-    <?php include("bottombit.php") ?>
