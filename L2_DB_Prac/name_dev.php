8ae11a80125303a37ecbd38ea70791d31fc02edd
kimmy2512 <51727300+kimmy2512@users.noreply.github.com>
2020-08-24 14:39:36 +1200

    working "labs" search

--- L2_DB_Prac/name_dev.php
+++ L2_DB_Prac/name_dev.php
@@ -5,7 +5,7 @@
     $find_sql = "SELECT * FROM `L2_prac_game_details` 
     JOIN `L2_prac_genre` ON (L2_prac_game_details.GenreID = L2_prac_genre.GenreID)
     JOIN `L2_prac_developer` ON (L2_prac_game_details.DeveloperID = L2_prac_developer.DeveloperID)
-    WHERE `Name` LIKE '%$name_dev%' OR 'DevName' LIKE '%$name_dev%'
+    WHERE `Name` LIKE '%$name_dev%' OR DevName LIKE '%$name_dev%'
     ";
     $find_query = mysqli_query($dbconnect, $find_sql);
     $find_rs = mysqli_fetch_assoc($find_query);
@@ -14,7 +14,7 @@
 ?>
 
         <div class="box main">
-            <h2>All Results</h2>
+            <h2>Name / Developer Results</h2>
             
             <?php 
             include ("results.php");
